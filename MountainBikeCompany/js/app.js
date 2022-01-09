df8db63e76bfeb09dede0f939133f8cebe77b418
Christian Barrionuevo <90290161+ChrisBarrio@users.noreply.github.com>
2022-01-09 15:20:48 -0300

    Add files via upload

---
+++ MountainBikeCompany/js/app.js
@@ -0,0 +1,104 @@
+// Tabs - scott
+const targets = document.querySelectorAll('[data-target]')
+const content = document.querySelectorAll('[data-content]')
+
+targets.forEach(target => {
+	target.addEventListener('click', () => {
+		content.forEach(c => {
+			c.classList.remove('active')
+		})
+		const t = document.querySelector(target.dataset.target)
+		t.classList.add('active')
+	})
+})
+
+// Carrousel - Index
+window.addEventListener('load', function(){
+	// Carousel de Venzo
+    new Glider(document.querySelector('.carousel__lista'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: '.carousel__indicadores',
+		arrows: {
+			prev: '.carousel__anterior',
+			next: '.carousel__siguiente'
+		},
+		responsive: [
+			{
+			  // screens greater than >= 775px
+			  breakpoint: 450,
+			  settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+				slidesToShow: 2,
+				slidesToScroll: 2
+			  }
+			},{
+			  // screens greater than >= 1024px
+			  breakpoint: 800,
+			  settings: {
+				slidesToShow: 3,
+				slidesToScroll: 1
+			  }
+			}
+		]
+	});
+    // Carousel de Merida
+    new Glider(document.querySelector('.carousel__lista2'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: '.carousel__indicadores2',
+		arrows: {
+			prev: '.carousel__anterior2',
+			next: '.carousel__siguiente2'
+		},
+		responsive: [
+			{
+			  // screens greater than >= 775px
+			  breakpoint: 450,
+			  settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+				slidesToShow: 2,
+				slidesToScroll: 2
+			  }
+			},{
+			  // screens greater than >= 1024px
+			  breakpoint: 800,
+			  settings: {
+				slidesToShow: 3,
+				slidesToScroll: 1
+			  }
+			}
+		]
+	});
+	// Carousel de Scott
+    new Glider(document.querySelector('.carousel__lista3'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: '.carousel__indicadores3',
+		arrows: {
+			prev: '.carousel__anterior3',
+			next: '.carousel__siguiente3'
+		},
+		responsive: [
+			{
+			  // screens greater than >= 775px
+			  breakpoint: 450,
+			  settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+				slidesToShow: 2,
+				slidesToScroll: 2
+			  }
+			},{
+			  // screens greater than >= 1024px
+			  breakpoint: 800,
+			  settings: {
+				slidesToShow: 3,
+				slidesToScroll: 1
+			  }
+			}
+		]
+	});
+});
+
+
+
